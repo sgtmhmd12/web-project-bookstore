71418f3ba1aee20a5ed56faa5bf034b9955cc658
mohamed <32130493@students.liu.edu.lb>
2025-11-22 16:34:54 +0200

    changed the text

--- src/component/Features.js
+++ src/component/Features.js
@@ -2,15 +2,8 @@ function Features() {
   return (
     <div>
       <h1>Features Page</h1>
-      <p>This page shows the main features of the project.</p>
-
-      <ul>
-        <li>✔ Functional Components</li>
-        <li>✔ React Router Navigation</li>
-        <li>✔ 5 Required Pages</li>
-        <li>✔ Navbar with Links</li>
-        <li>✔ Clean simple React structure</li>
-      </ul>
+      <p>Our bookstore includes a curated collection of popular and highly recommended titles. Each book contains essential information such as the title, author, category, rating, and price. Users can browse all books, search by title, and filter by category to quickly find what they need.</p>
+      
     </div>
   );
 }
