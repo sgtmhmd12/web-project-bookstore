1a06da3c32c2ed0ac3f43e1fc8cfd063f66ff028
mohamed <32130493@students.liu.edu.lb>
2025-12-28 15:14:44 +0200

    Fix mobile navbar toggle

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -1,56 +1,45 @@
 import { Link } from "react-router-dom";
-import { auth } from "../firebase";
 
 function Navbar() {
-  const isAdmin = auth.currentUser?.email === "admin@bookstore.com";
-
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
+        {/* BRAND */}
         <Link className="navbar-brand fw-bold text-info" to="/">
           BookStore
         </Link>
 
+        {/* TOGGLER BUTTON (VERY IMPORTANT) */}
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navMenu"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navMenu">
-          <ul className="navbar-nav ms-auto gap-2">
+        {/* COLLAPSIBLE MENU */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
 
             <li className="nav-item">
               <Link className="nav-link" to="/books">Books</Link>
             </li>
-
-            {isAdmin && (
-              <li className="nav-item">
-                <Link className="nav-link" to="/books/add">
-                  Add Book
-                </Link>
-              </li>
-            )}
 
             <li className="nav-item">
               <Link className="nav-link" to="/cart">Cart</Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/features">Features</Link>
+              <Link className="nav-link" to="/login">Login</Link>
             </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contact</Link>
-            </li>
-
           </ul>
         </div>
       </div>
