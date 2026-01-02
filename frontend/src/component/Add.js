4b9e9523c5a6365e6f20d909bf861f63daf2e1f6
mohamed <32130493@students.liu.edu.lb>
2026-01-02 22:05:00 +0200

    fix

--- frontend/src/component/Add.js
+++ frontend/src/component/Add.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { API_URL } from "../api";
 
+
 const Add = () => {
   const [book, setBook] = useState({
     title: "",
